058a5b0f8d1b67167c741abf7b778e396a2506cf
ricksterhd123 <ricksterhd123@gmail.com>
2022-06-19 16:09:37 +0000

    Update config

--- ytmp3/config.py
+++ ytmp3/config.py
@@ -1,12 +1,11 @@
 config = {
     "filepath": "../static",
     "log_path": "../log/bot.log",
-    "youtube_api_key": "",
     "bot_token": "",
-    "hostname": "https://localhost:8080/play",
+    "hostname": "",
     "max_duration": {
         # indexed by role id
         # fallback
-        'default': 30,
+        'default': 90,
     },
 }
